b6d8b9071aac4c86cf2c917b30ddf29f7e5ddb40
ycindy <cindy.yu.cy@gmail.com>
2015-08-04 23:13:59 -0500

    slides work now yay

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,42 @@
+var makeSlideshow = function(selector, width) {
+  selector.bjqs({
+    animtype      : 'slide',
+    height        : '100vh',
+    width         : width,
+    responsive    : true,
+    showmarkers   : false, 
+    keyboardnav   : true, 
+    hoverpause    : true,
+    automatic     : false,
+    nexttext      : '&nbsp;', 
+    prevtext      : ' ', 
+    usecaptions   : false,
+    responsive    : true,
+    animduration  : 300
+  });
+};
+
 $(document).ready(function($) {
 
   var width = $(document).width();
 
-  var makeSlideshow = function(selector) {
-    selector.bjqs({
-      animtype      : 'slide',
-      height        : '100vh',
-      width         : width,
-      responsive    : true,
-      showmarkers   : false, 
-      keyboardnav   : true, 
-      hoverpause    : true,
-      automatic     : false,
-      nexttext      : '&nbsp;', 
-      prevtext      : ' '
-    });
-  };
+  makeSlideshow($('#speakers'), width);
+  makeSlideshow($('#activities'), width);
 
-  makeSlideshow($('#speakers'));
-  makeSlideshow($('#activities'));
+  $(window).resize(function() {
+    var width = $(window).width();
+    $('.slideshow').each(function() {
+      var numSlides = $(this).find('li').length; 
+      $(this).css('max-width', width + 'px');
+      $(this).find('li.speaker.bjqs-slide').css('width', width + 'px');
+      $(this).find('.bjqs').css('width', width + 'px');
+      $(this).find('div.bjqs-wrapper').css('width', width + 'px');
+      $(this).find('ul.bjqs').css({
+        width: (width * (numSlides))  + 'px'
+      });
+      $(this).find('ul li').css('width', width + 'px');
+    });    
+  });
   
 });
+
