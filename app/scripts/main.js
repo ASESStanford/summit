0704d87a99813a830d36ab73c77d7e8b6a054f12
ycindy <cindy.yu.cy@gmail.com>
2015-08-11 23:57:19 -0500

    wip

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,6 +18,19 @@ var makeSlideshow = function(selector, width) {
 
 $(document).ready(function($) {
 
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+  
   var width = $(document).width();
 
   makeSlideshow($('#speakers'), width);
